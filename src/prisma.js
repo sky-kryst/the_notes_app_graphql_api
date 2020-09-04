d557bbc4a40b9e72c6c83cfe2f630519ed77986b
Aakash <aakashmeshram@outlook.com>
2020-09-05 02:03:47 +0530

    Making envs secure

--- src/prisma.js
+++ src/prisma.js
@@ -4,7 +4,7 @@ import { fragmentReplacements } from './Resolvers'
 const prisma = new Prisma({
   typeDefs: `${__dirname}/generated/prisma.graphql`,
   endpoint: process.env.PRISMA_ENDPOINT,
-  secret: 'thisismysupersecrettext',
+  secret: process.env.PRISMA_SECRET,
   fragmentReplacements,
 })
 
